4d74b2f0a67f5ae3694f42a3277b3b845f995142
Kasjan Łabój <kasjanlaboj@gmail.com>
2024-12-18 13:34:00 +0100

    JobPostService logic

---
+++ Application/JobPost/IJobPostService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.JobPost
+{
+    public interface IJobPostService
+    {
+        Task<IEnumerable<Domain.JobPost.JobPost>> GetAllAsync();
+        Task<Domain.JobPost.JobPost> GetByIdAsync(int id);
+        Task AddAsync(Domain.JobPost.JobPost entity);
+        Task UpdateAsync(Domain.JobPost.JobPost entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Application/JobPost/JobPostService.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.JobPost
+{
+    public class JobPostService : IJobPostService
+    {
+        private readonly IRepository<Domain.JobPost.JobPost> _repository;
+        public JobPostService(IRepository<Domain.JobPost.JobPost> repository) 
+        { 
+            _repository = repository;
+        }
+        public async Task AddAsync(Domain.JobPost.JobPost entity)
+        {
+            await _repository.AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<Domain.JobPost.JobPost>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Domain.JobPost.JobPost> GetByIdAsync(int id)
+        {
+            return await GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(Domain.JobPost.JobPost entity)
+        {
+            await UpdateAsync(entity);
+        }
+    }
+}
