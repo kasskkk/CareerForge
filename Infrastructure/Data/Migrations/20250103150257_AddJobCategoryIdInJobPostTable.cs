263014298ae5aaef0cc4edcc683529cfb41e2c72
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-03 16:17:47 +0100

    add job category id in job post model

---
+++ Infrastructure/Data/Migrations/20250103150257_AddJobCategoryIdInJobPostTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobCategoryIdInJobPostTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JobCategoryId",
+                table: "JobPosts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPosts_JobCategoryId",
+                table: "JobPosts",
+                column: "JobCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobPosts_JobCategories_JobCategoryId",
+                table: "JobPosts",
+                column: "JobCategoryId",
+                principalTable: "JobCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobPosts_JobCategories_JobCategoryId",
+                table: "JobPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobPosts_JobCategoryId",
+                table: "JobPosts");
+
+            migrationBuilder.DropColumn(
+                name: "JobCategoryId",
+                table: "JobPosts");
+        }
+    }
+}
