bc174a5b2769b1515c44a71e9d201464a90cd2be
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-04 14:45:39 +0100

    job category repository

---
+++ Infrastructure/JobPost/JobCateogryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Generic;
+using Domain.JobPost;
+using Infrastructure.Data;
+using Infrastructure.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.JobPost
+{
+    public class JobCateogryRepository : GenericRepository<JobCategory>
+    {
+        public JobCateogryRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
