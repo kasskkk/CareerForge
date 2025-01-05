c77f19beb11b2295ceb54341e053af2c5667136a
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-05 17:34:41 +0100

    added interface implementation in job category repository

---
+++ Domain/JobPost/IJobCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.JobPost
+{
+    public interface IJobCategoryRepository : IRepository<JobCategory>
+    {
+    }
+}
---
+++ Infrastructure/JobPost/JobCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Generic;
+using Domain.JobPost;
+using Infrastructure.Data;
+using Infrastructure.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.JobPost
+{
+    public class JobCategoryRepository : GenericRepository<JobCategory> , IJobCategoryRepository
+    {
+        public JobCategoryRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
