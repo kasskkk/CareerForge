cb40987175feba871c23ae2026aae852db72895f
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-09 16:49:35 +0100

    add model for reported job posts

---
+++ Domain/ReportedPost/ReportedJobPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ReportedPost
+{
+    public class ReportedJobPost
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int JobPostId { get; set; }
+        [ForeignKey(nameof(JobPostId))]
+        public Domain.JobPost.JobPost JobPost { get; set; }
+    }
+}
