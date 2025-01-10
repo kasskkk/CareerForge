3caa8c5b32b9598a700df55cbf4a9b94e772d79e
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-10 09:20:54 +0100

    add interface for report service

---
+++ Application/ReportedPostService/IReportedJobPostService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ReportedPostService
+{
+    public interface IReportedJobPostService
+    {
+        Task<IEnumerable<Domain.ReportedPost.ReportedJobPost>> GetAllAsync();
+        Task<Domain.ReportedPost.ReportedJobPost> GetByIdAsync(int id);
+        Task AddAsync(Domain.ReportedPost.ReportedJobPost entity);
+        Task UpdateAsync(Domain.ReportedPost.ReportedJobPost entity);
+        Task DeleteAsync(int id, ClaimsPrincipal user);
+    }
+}
