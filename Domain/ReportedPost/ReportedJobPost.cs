0c080724ea7505dc6e55169c6f968f4ca8df32b7
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-10 09:17:54 +0100

    add report post model

--- Domain/ReportedPost/ReportedJobPost.cs
+++ Domain/ReportedPost/ReportedJobPost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enum;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,7 +13,7 @@ namespace Domain.ReportedPost
     {
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public ReportName Name { get; set; }
         [Required]
         public string Description { get; set; }
         [Required]
