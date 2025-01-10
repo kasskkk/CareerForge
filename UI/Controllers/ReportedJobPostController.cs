fc444afa6d9699a74538eb7891a6401a42946b7c
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-10 09:21:19 +0100

    add report controller

---
+++ UI/Controllers/ReportedJobPostController.cs
@@ -0,0 +1,20 @@
+﻿using Application.ReportedPostService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class ReportedJobPostController : Controller
+    {
+        private readonly IReportedJobPostService _service;
+        public ReportedJobPostController(IReportedJobPostService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var reportedPosts = await _service.GetAllAsync();
+
+            return View(reportedPosts);
+        }
+    }
+}
