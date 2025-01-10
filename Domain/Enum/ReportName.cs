a94f8060b2240454e0e2b7e4092f6766a6980df7
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-10 09:17:40 +0100

    enum names for reports

---
+++ Domain/Enum/ReportName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enum
+{
+    public enum ReportName
+    {
+        Fraud,
+        Impersonation,
+        InappropriateContent,
+        Spam,
+        Other
+    }
+}
