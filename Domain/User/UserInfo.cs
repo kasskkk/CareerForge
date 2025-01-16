cfa0e03dd561c755f1a1328a7e0b08cd7cab84af
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-16 21:05:10 +0100

    add user info model

---
+++ Domain/User/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.User
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
+        [Required]
+        public string CvFilePath { get; set; }
+    }
+}
