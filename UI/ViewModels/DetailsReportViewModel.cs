ef9a3bb450268a834ecc09568d83e5b03a6023ca
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-16 17:23:17 +0100

    view controller for details

---
+++ UI/ViewModels/DetailsReportViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Enum;
+using Domain.JobPost;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace UI.ViewModels
+{
+    public class DetailsReportViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Description { get; set; }
+        public string Salary { get; set; }
+        public string Location { get; set; }
+        public int JobCategoryId { get; set; }
+        public IEnumerable<SelectListItem>? Categories { get; set; }
+        public DateTime Created { get; set; }
+        public JobCategory Category { get; set; }
+        [Required]
+        public ReportName ReportName { get; set; }
+        [Required]
+        public string ReportDescription { get; set; }
+    }
+}
