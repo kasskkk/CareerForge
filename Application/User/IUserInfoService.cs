a7c36d71e701f468f7241ef79dd7d1bf3333ac55
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-17 13:26:54 +0100

    add service for user info

---
+++ Application/User/IUserInfoService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public interface IUserInfoService
+    {
+        Task<IEnumerable<UserInfo>> GetAllAsync();
+        Task<UserInfo> GetByIdAsync(int id);
+        Task AddAsync(UserInfo entity);
+        Task UpdateAsync(UserInfo entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Application/User/UserInfoService.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Generic;
+using Domain.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public class UserInfoService : IUserInfoService
+    {
+        private readonly IRepository<UserInfo> _userInfoRepository;
+        public UserInfoService(IRepository<UserInfo> userInfoRepository)
+        {
+            _userInfoRepository = userInfoRepository;
+        }
+        public async Task AddAsync(UserInfo entity)
+        {
+            await _userInfoRepository.AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var info = await _userInfoRepository.GetByIdAsync(id);
+
+            await _userInfoRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<UserInfo>> GetAllAsync()
+        {
+            return await _userInfoRepository.GetAllAsync();
+        }
+
+        public async Task<UserInfo> GetByIdAsync(int id)
+        {
+            return await _userInfoRepository.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(UserInfo entity)
+        {
+            await _userInfoRepository.UpdateAsync(entity);
+        }
+    }
+}
