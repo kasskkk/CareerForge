8fdacb7e0e91684ab3dfbac71519f1b60fa743ad
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-17 17:59:47 +0100

    add migration and on model creating for cascade delete

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -19,5 +19,23 @@ namespace Infrastructure.Data
         public DbSet<JobApplication> JobApplications { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfiguracja kaskadowego usuwania dla klucza obcego UserId w tabeli JobApplications
+            modelBuilder.Entity<JobApplication>()
+                .HasOne(ja => ja.User)
+                .WithMany()
+                .HasForeignKey(ja => ja.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Wyłącz kaskadowe usuwanie dla klucza obcego JobPostId w tabeli JobApplications
+            modelBuilder.Entity<JobApplication>()
+                .HasOne(ja => ja.JobPost)
+                .WithMany(jp => jp.JobApplications)
+                .HasForeignKey(ja => ja.JobPostId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
---
+++ Infrastructure/Data/Migrations/20250117164541_AddJobApplicationTableWithNaviagtion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobApplicationTableWithNaviagtion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    JobPostId = table.Column<int>(type: "int", nullable: false),
+                    ApplicationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobApplications_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobApplications_JobPosts_JobPostId",
+                        column: x => x.JobPostId,
+                        principalTable: "JobPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_JobPostId",
+                table: "JobApplications",
+                column: "JobPostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_UserId",
+                table: "JobApplications",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+        }
+    }
+}
