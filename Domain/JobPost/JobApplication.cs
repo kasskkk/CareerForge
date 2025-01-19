05cad41eed6dffb47908a4ed5cef3cbfe503e333
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-19 18:52:13 +0100

    add job application model

---
+++ Domain/JobPost/JobApplication.cs
@@ -0,0 +1,25 @@
+﻿using Domain.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.JobPost
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserInfoId { get; set; }
+        [ForeignKey(nameof(UserInfoId))]
+        public UserInfo UserInfo { get; set; }
+        [Required]
+        public int JobPostId { get; set; }
+        [ForeignKey(nameof(JobPostId))]
+        public JobPost JobPost { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
