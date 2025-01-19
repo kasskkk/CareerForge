dbccbedf6c85c44653837767edc91026b86f2ecf
Kasjan Łabój <kasjanlaboj@gmail.com>
2025-01-19 18:52:26 +0100

    add job application service and interface

---
+++ Application/JobApplication/IJobApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.JobApplication
+{
+    public interface IJobApplicationService
+    {
+        Task<IEnumerable<Domain.JobPost.JobApplication>> GetAllAsync();
+        Task<Domain.JobPost.JobApplication> GetByIdAsync(int id);
+        Task AddAsync(Domain.JobPost.JobApplication entity);
+        Task UpdateAsync(Domain.JobPost.JobApplication entity);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Domain.JobPost.JobApplication>> GetJobApplicationsByJobPostId(int id);
+    }
+}
---
+++ Application/JobApplication/JobApplicationService.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Generic;
+using Domain.JobPost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.JobApplication
+{
+    public class JobApplicationService : IJobApplicationService
+    {
+        private readonly IRepository<Domain.JobPost.JobApplication> _repository;
+        public JobApplicationService(IRepository<Domain.JobPost.JobApplication> repository)
+        {
+            _repository = repository;
+        }
+        public async Task AddAsync(Domain.JobPost.JobApplication entity)
+        {
+            await _repository.AddAsync(entity);
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Domain.JobPost.JobApplication>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.JobPost.JobApplication> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Domain.JobPost.JobApplication>> GetJobApplicationsByJobPostId(int id)
+        {
+            var jobApplications = await _repository.GetAllAsync();
+
+            return jobApplications.Where(ja => ja.JobPostId == id);
+        }
+
+        public Task UpdateAsync(Domain.JobPost.JobApplication entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
